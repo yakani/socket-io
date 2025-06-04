eafea633ec979de5f5e30defceff281e69d2cc9d
main <yakamdimitri@gmail.com>
2025-06-04 14:38:12 +0200

    damai

--- fronte/src/component/Messageinput.jsx
+++ fronte/src/component/Messageinput.jsx
@@ -89,7 +89,7 @@ const Messageinput = () => {
 
         <button
           type="button"
-          className={`hidden sm:flex btn btn-circle
+          className={`flex btn btn-circle
                    ${imagePreview ? "text-emerald-500" : "text-zinc-400"}`}
           onClick={() => fileInputRef.current?.click()}
         >
